a162fac6f6446afb2fbc7f9ba97abc43cdc9abef
antos07 <trotsenko.ant07@gmail.com>
2023-11-13 19:39:41 +0100

    Implement basic AST TreeNode

---
+++ src/ast/TreeNode.hpp
@@ -0,0 +1,36 @@
+//
+// Created by antos07 on 11/13/23.
+//
+
+#ifndef SYSPROG_LAB5_SRC_AST_TREENODE_HPP_
+#define SYSPROG_LAB5_SRC_AST_TREENODE_HPP_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <initializer_list>
+
+namespace ast {
+
+class TreeNode {
+  std::string name_;
+  std::vector<std::unique_ptr<TreeNode>> children_;
+
+ public:
+  explicit TreeNode(std::string name);
+
+  TreeNode(std::string name, std::vector<std::unique_ptr<TreeNode>> children);
+
+  void append_child(std::unique_ptr<TreeNode> &&child);
+
+  void print(std::ostream &out) const;
+
+ private:
+  void print(std::ostream &out, std::size_t indent_size) const;
+};
+
+std::unique_ptr<TreeNode> make_node(std::string name);
+}
+
+#endif //SYSPROG_LAB5_SRC_AST_TREENODE_HPP_
